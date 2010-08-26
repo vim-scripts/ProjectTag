0af9a9813bab789ed204071b451cfd18b9fe2781
H Xu <xuhdev@gmail.com>
2010-08-26 00:00:00 +0000

    Version 0.1: Initial upload

---
+++ ProjectTag/ProjectTag.py
@@ -0,0 +1,227 @@
+# File: ProjectTag/ProjectTag.py
+# Version: 0.1
+
+import sys
+import re
+import os
+import ConfigParser
+import subprocess
+import tempfile
+import shutil
+import vim
+
+
+# search for the file from current directory to upper dir until meet the root directory,
+# return the file's absolute path if found, or return None if not found
+def search_for_file_upper( cur_dir, file_name ):
+
+    cur_dir_local = os.path.abspath( cur_dir )
+    
+    while 1:
+        cur_check = cur_dir_local + os.path.sep + file_name
+        if os.path.isfile( cur_check ): # if cur path meets the condition, then return the path
+            return cur_check
+
+        # if condition does not meet, go upper
+        upper_cur_dir_local = os.path.dirname( cur_dir_local )
+        if upper_cur_dir_local == cur_dir_local: # if this is the root directory
+            return None
+
+        cur_dir_local = upper_cur_dir_local
+
+
+
+# get include files (#include <somefile> or #include "somefile")
+# from a list of file lines
+def get_included_files(filelines): 
+
+    ret = set()
+
+    rematch = re.compile(r'#include[ \t]*[<"][^<>"]+[>"]')
+    refind = re.compile(r'(?<=[<"])[^<>"]+(?=[>"])')
+
+    for line in filelines:
+        trimmed_line = line.strip()
+        rem = rematch.match(trimmed_line)
+
+        if rem == None:
+            continue
+
+        rem = refind.search(trimmed_line);
+        if rem == None:
+            continue
+
+        ret.add( rem.group(0) )
+
+    return ret
+
+# get the included file of a c/c++ source file and it's included file(internal use)
+def __get_included_files_reclusively(src, include_dirs, checked_files):
+    # if current file has been checked, return an empty set
+    if src in checked_files:
+        return set()
+    
+    # set current file as checked
+    checked_files.add(src)
+
+    try:
+        f = open(src,'r')
+    except IOError, message:
+        print >> sys.stderr, "can not open file "+src, message
+        return set()
+
+    ret = set()
+
+    # get the included file paths
+    header_files = get_included_files( f.readlines() )
+    for include_dir in include_dirs:
+        for header_file in header_files:
+
+            file_path = include_dir + os.path.sep + header_file # the path of new file
+
+            # make the file path seperator be consitent with the os'
+            if os.path.sep == '/':
+                file_path = file_path.replace('\\','/')
+            else:
+                file_path = file_path.replace('/','\\')
+
+            # if the file exists, then add this file to ret
+            if os.path.isfile(file_path) :
+                ret.add(file_path)
+                ret |= __get_included_files_reclusively(file_path, include_dirs, checked_files)
+
+    return ret
+
+# get the included file of a c/c++ source file and it's included file
+def get_included_files_reclusively(src, include_dirs):
+    return __get_included_files_reclusively(src, include_dirs, set())
+
+
+
+# generate tags from file_set, write the result to outfile
+# params: 
+# file_set: file to generate tags for
+# outfile: the output file
+# tag_prog_cmd: command to call the tag program, such as /usr/bin/ctags, etc
+# flags: tag generation flags
+def generate_tags_ctags(file_set, outfile, flags, tag_prog_cmd='ctags'):
+    # append every file to the outfile
+    for fi in file_set:
+        subprocess.call(tag_prog_cmd + ' ' + flags + ' --append ' + '-f '+ outfile + ' ' + fi, shell=True)
+
+    return 0
+
+
+# the config parser for project ini file
+class ProjectConfig(ConfigParser.ConfigParser):
+    # init, the argument project_file_name is the name of project file, not the full path. the function will
+    # search upper to find this file
+
+    # the directory where the project file locates
+    project_dir = ''
+
+    def __init__( self, project_file_name ):
+
+        ConfigParser.ConfigParser.__init__(self)
+
+        project_file_path = search_for_file_upper( '.', project_file_name )
+        if project_file_path == None:
+            return
+
+        self.project_dir = os.path.dirname( project_file_path )
+
+        self.read( project_file_path )
+        self.set_project_config_parser_default_value()
+
+
+    # generate tags
+    def generate_tags(self):
+        tagoutput = self.get('general', 'tagoutput')
+        tagoutput = self.project_dir + os.path.sep + tagoutput
+        tagflag = self.get('general', 'tagflag')
+        tagprog = self.get('general', 'tagprog')
+        temp_tagoutput = tagoutput + '.tmp'
+        generate_tags_ctags(self.get_files_to_tag(), temp_tagoutput, tagflag, tagprog)
+        
+        if os.path.isfile(tagoutput):
+            try:
+                os.remove(tagoutput)
+            except OSError, message:
+                print >> sys.stderr, 'Can not delete '+tagoutput, message
+                return False
+
+        shutil.move(temp_tagoutput, tagoutput)
+
+        return True
+        
+
+    # set the option to default value if the user does not set it
+    def __set_option_if_not_have(self, section, option, defaultval):
+        if not self.has_section(section):
+            self.add_section(section)
+
+        if not self.has_option(section, option):
+            self.set(section, option, defaultval)
+
+        return
+
+    # find the tags output file and add it to tags
+    def add_tag_file(self):
+        # do nothing if project_dir is not defined
+        if self.project_dir == '':
+            return
+
+        tagoutput = self.get( 'general', 'tagoutput' ).strip()
+        tag_path = self.project_dir + os.path.sep + tagoutput
+        vim.command('setlocal tags+='+tag_path)
+
+
+    def set_project_config_parser_default_value(self):
+        self.__set_option_if_not_have('general','include_dirs','')
+        self.__set_option_if_not_have('general','sources','')
+        self.__set_option_if_not_have('general','tagprog','ctags')
+        self.__set_option_if_not_have('general','tagflag','--c-kinds=+px --c++-kinds=+px')
+        self.__set_option_if_not_have('general','tagoutput','tags.prom')
+        
+    # return a list of files to tag
+    def get_files_to_tag(self):
+        sources = [s.strip() for s in self.get('general','sources').split(',')]
+        include_dirs = [s.strip() for s in self.get('general','include_dirs').split(',')]
+        ret = set()
+
+        # the full path of sources
+        sources_full_path = [ self.project_dir + os.path.sep + s for s in sources ]
+
+        # first add the sources
+        ret |= set(sources_full_path)
+
+        # now it's time to get the path of all included headers
+        for src in sources_full_path:
+            # if it's not a C/C++ source file, then skip
+            if re.match(r'.*\.(c|C|cpp|cxx|cc|c\+\+)',src) == None:
+                continue
+
+            ret |= get_included_files_reclusively(src, include_dirs)
+
+
+        return ret
+
+
+        
+
+#pp = ProjectConfig('test.ini')
+#print pp.get_files_to_tag()
+
+
+#try:
+    #f = open("DataViewAnyRenderer.cpp","r")
+#except IOError, message:
+    #print >> sys.stderr, "File open error:", message
+    #sys.exit(1)
+#print get_included_files(f.readlines())
+#pp = ProjectConfig()
+#pp.read('full.guil')
+#pp.set_project_config_parser_default_value()
+##print pp.items('general')
+#print pp.get_files_to_tag()
+#pp.generate_tags()
---
+++ plugin/ProjectTag.vim
@@ -0,0 +1,102 @@
+" File: plugin/ProjectTag.vim
+" Version: 0.1
+
+if v:version < 700
+    finish
+endif
+
+if !has('python')
+    finish
+endif
+
+" check whether this script is already loaded
+if exists("g:loaded_ProjectTag")
+    finish
+endif
+let g:loaded_ProjectTag = 1
+
+let s:saved_cpo = &cpo
+set cpo&vim
+
+" initialization {{{1
+let s:py_dir = substitute(findfile('ProjectTag/ProjectTag.py', &rtp), '/ProjectTag.py', '', '')
+
+let s:default_project_name = 'project.prom'
+
+python << EEOOFF
+
+import sys
+import vim
+import os
+
+# add $VIMRUNTIME/ProjectTag to module search directory
+sys.path.append( os.path.abspath( vim.eval('s:py_dir')) )
+
+# import required libraries
+import ProjectTag
+import threading
+
+# used to restore global varibles
+class ProjectTagGlobal:
+    # the ctags thread
+    tag_thread = None
+
+# add the ctag file
+pc = ProjectTag.ProjectConfig( vim.eval('s:default_project_name') )
+pc.add_tag_file()
+del pc
+
+EEOOFF
+
+" autocmd {{{1
+" automatically add the tags file when entering a buffer
+autocmd BufEnter * python ProjectTag.ProjectConfig( vim.eval('s:default_project_name') ).add_tag_file()
+
+" functions {{{1
+
+" generate tags
+function s:GenerateProjectTags( back_ground )
+
+python << EEOOFF
+pc = ProjectTag.ProjectConfig( vim.eval('s:default_project_name') )
+
+if ProjectTagGlobal.tag_thread != None and ProjectTagGlobal.tag_thread.isAlive():
+    vim.command('return')
+
+ProjectTagGlobal.tag_thread = threading.Thread( target=ProjectTag.ProjectConfig.generate_tags, args=(pc,) )
+ProjectTagGlobal.tag_thread.daemon = True
+ProjectTagGlobal.tag_thread.start()
+
+EEOOFF
+    
+    " if run foreground
+    if a:back_ground == 0
+
+python << EEOOFF
+
+ProjectTagGlobal.tag_thread.join()
+
+EEOOFF
+
+    endif
+
+python << EEOOFF
+
+# after generating the tag, add the tag file to tags
+pc.add_tag_file()
+
+EEOOFF
+
+endfunction
+
+
+" commands {{{1
+command GenProTags call s:GenerateProjectTags(0)
+command GenProTagsBg call s:GenerateProjectTags(1)
+
+" }}}
+
+
+let &cpo = s:saved_cpo
+
+" vim: fdm=marker
